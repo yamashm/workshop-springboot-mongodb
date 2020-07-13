0c454b0b4dcd803de08a43c552149c387e717699
yamashm <awnmist@gmail.com>
2020-07-12 21:05:44 -0300

    Consulta com vários critérios

--- src/main/java/com/example/workshopmongo/services/PostService.java
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,5 +30,11 @@ public class PostService {
 	public List<Post> findByTitle(String text){
 		//return repo.findByTitleContainingIgnoreCase(text);
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 *60 *60 *1000);
+		
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
